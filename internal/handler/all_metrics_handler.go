a595a5f0d65c3f7b13b521dfefd8025b13e0046b
Ilia Onishchenko <sinoayli@gmail.com>
2025-11-29 16:58:33 +0200

    [iter3] feat: introduce chi and additional endpoints

---
+++ internal/handler/all_metrics_handler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"fmt"
+	models "github.com/iliaonishchenko/aggreg8/internal/model"
+	"github.com/iliaonishchenko/aggreg8/internal/service"
+	"net/http"
+	"strings"
+)
+
+type AllMetricsHandler struct {
+	storage service.MetricStorage
+}
+
+func NewAllMetricsHandler(s service.MetricStorage) *AllMetricsHandler {
+	return &AllMetricsHandler{
+		storage: s,
+	}
+}
+
+func (amh *AllMetricsHandler) HandleAll(w http.ResponseWriter, r *http.Request) {
+	metrics := amh.storage.GetAllMetrics()
+
+	var html strings.Builder
+	html.WriteString("<html><head><title>Metrics</title></head><body>")
+	html.WriteString("<h1>All Metrics</h1>")
+	html.WriteString("<table border='1' cellpadding='10'>")
+	html.WriteString("<tr><th>Name</th><th>Type</th><th>Value</th></tr>")
+
+	for _, metric := range metrics {
+		html.WriteString("<tr>")
+		html.WriteString(fmt.Sprintf("<td>%s</td>", metric.ID))
+		html.WriteString(fmt.Sprintf("<td>%s</td>", metric.MType))
+
+		if metric.MType == models.Gauge && metric.Value != nil {
+			html.WriteString(fmt.Sprintf("<td>%g</td>", *metric.Value))
+		} else if metric.MType == models.Counter && metric.Value != nil {
+			html.WriteString(fmt.Sprintf("<td>%d</td>", int64(*metric.Value)))
+		} else if metric.Delta != nil {
+			html.WriteString(fmt.Sprintf("<td>%d</td>", *metric.Delta))
+		} else {
+			html.WriteString("<td>-</td>")
+		}
+
+		html.WriteString("</tr>")
+	}
+
+	html.WriteString("</table></body></html>")
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(html.String()))
+}
--- internal/service/storage.go
+++ internal/service/storage.go
@@ -1,38 +1,66 @@
 package service
 
+import (
+	"errors"
+	models "github.com/iliaonishchenko/aggreg8/internal/model"
+)
+
 type MetricStorage interface {
-	UpdateGauge(name string, value float64) error
-	UpdateCounter(name string, value int64) error
-	GetGauge(name string) (float64, bool)
-	GetCounter(name string) (int64, bool)
+	UpdateMetric(metric *models.Metrics) bool
+	GetMetric(metricName string) (*models.Metrics, error)
+	GetAllMetrics() []*models.Metrics
 }
 
 type MemStorage struct {
-	gauges   map[string]float64
-	counters map[string]int64
+	metrics map[string]*models.Metrics
 }
+
+var ErrMetricNotFound = errors.New("metric not found")
 
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
-		gauges:   make(map[string]float64),
-		counters: make(map[string]int64),
+		metrics: make(map[string]*models.Metrics),
 	}
 }
 
-func (ms *MemStorage) UpdateGauge(name string, value float64) error {
-	ms.gauges[name] = value
-	return nil
-}
-func (ms *MemStorage) UpdateCounter(name string, value int64) error {
-	ms.counters[name] += value
-	return nil
-}
-func (ms *MemStorage) GetGauge(name string) (float64, bool) {
-	value, exists := ms.gauges[name]
-	return value, exists
+func (ms *MemStorage) UpdateMetric(metric *models.Metrics) bool {
+	switch metric.MType {
+	case models.Gauge:
+		prev, exists := ms.metrics[metric.ID]
+		if !exists {
+			ms.metrics[metric.ID] = metric
+			return true
+		}
+
+		prev.Value = metric.Value
+		return true
+	case models.Counter:
+		prev, exists := ms.metrics[metric.ID]
+		if !exists {
+			ms.metrics[metric.ID] = metric
+			return true
+		}
+		newValue := *prev.Value + *metric.Value
+		prev.Value = &newValue
+		return true
+	default:
+		return false
+	}
 }
 
-func (ms *MemStorage) GetCounter(name string) (int64, bool) {
-	value, exists := ms.counters[name]
-	return value, exists
+func (ms *MemStorage) GetMetric(metricName string) (*models.Metrics, error) {
+	metric, exists := ms.metrics[metricName]
+	if !exists {
+		return nil, ErrMetricNotFound
+	}
+	return metric, nil
+}
+
+func (ms *MemStorage) GetAllMetrics() []*models.Metrics {
+	results := make([]*models.Metrics, 0, len(ms.metrics))
+	for _, v := range ms.metrics {
+		results = append(results, v)
+	}
+
+	return results
 }
