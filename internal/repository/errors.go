dfeb9dc16b6234e61d6553b772394aae2e05a8ff
Ilia Onishchenko <sinoayli@gmail.com>
2026-01-23 17:59:28 +0200

    [iter-13] add error handling for server

---
+++ internal/repository/errors.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type PostgresErrorClassifier struct{}
+
+func NewPostgresErrorClassifier() *PostgresErrorClassifier {
+	return &PostgresErrorClassifier{}
+}
+
+func (c *PostgresErrorClassifier) isRetriable(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return len(pgErr.Code) >= 2 && pgErr.Code[:2] == "08"
+	}
+
+	return false
+}
