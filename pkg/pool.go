33bb6949568fa1f190f6279d2ea9d57677e7878c
Ilia Onishchenko <sinoayli@gmail.com>
2026-04-29 00:56:01 +0300

    [iter-22] add pool custom implementation

---
+++ pkg/pool.go
@@ -0,0 +1,40 @@
+package pkg
+
+import "sync"
+
+// generate:reset
+type Pool[T Resetable] struct {
+	values []T
+	m      sync.Mutex
+}
+
+type Resetable interface {
+	Reset()
+}
+
+func New[T Resetable]() *Pool[T] {
+	return &Pool[T]{
+		values: make([]T, 0),
+	}
+}
+func (p *Pool[T]) Get() T {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	var zero T
+	n := len(p.values)
+	if n == 0 {
+		return zero
+	}
+	last := p.values[n-1]
+	p.values = p.values[:n-1]
+	return last
+
+}
+
+func (p *Pool[T]) Put(t T) {
+	t.Reset()
+	p.m.Lock()
+	defer p.m.Unlock()
+	p.values = append(p.values, t)
+}
